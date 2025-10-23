531ef50c90fb737a42c0a13d97059fc6c3318749
Teodor Glisic <teodor.glisic@students.fhnw.ch>
2025-10-23 11:59:53 +0200

    AB#15 Creating catalog application

---
+++ src/main/java/ch/fhnw/students/almcatalog/AlmCatalogApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.students.almcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlmCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/fhnw/students/almcatalog/AlmCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.students.almcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
